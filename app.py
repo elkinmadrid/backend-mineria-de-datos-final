a6cfdf3a6e7940ec142d5d088dea917459d112be
elkinmadrid <elkinamp@gmail.com>
2023-05-04 22:26:37 -0500

    Cors enable

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 from api.dataset.route import dataset
 import os
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
 
 from api.users.route import usuarios
 app.register_blueprint(usuarios)
@@ -11,4 +13,6 @@ app.register_blueprint(dataset)
 
 JWT_SECRETKEY = os.environ['JWT_SECRETKEY']
 app.config['SECRET_KEY']=JWT_SECRETKEY
-app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(debug=True)
